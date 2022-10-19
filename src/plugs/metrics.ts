925bc3ebf49c1db9285266561b5b268649183c7e
Kuhle Hanisi <hanisikuhle@gmail.com>
2022-10-19 23:00:31 +0200

    Updated first version

--- src/plugs/metrics.ts
+++ src/plugs/metrics.ts
@@ -12,18 +12,23 @@ function matchFind(content: string, matchRegex: RegExp) {
 }
 
 const METRICS: MetricTypes = {
+	// Total number of comments that are shorter than 15 characters
 	shorter_than_15: (content: string) => {
 		return content.length < 15;
 	},
+	// Total number of comments that refer to the "Mover" device
 	mover: (content: string) => {
 		return matchFind(content, /mover/gi);
 	},
+	// Total number of comments that refer to the "Shaker" device
 	shaker: (content: string) => {
 		return matchFind(content, /shaker/gi);
 	},
+	// Total number of comments that contain one on more question marks "?"
 	questions: (content: string) => {
 		return content.includes("?");
 	},
+	// Total number of comments that contain a URL to a web page
 	spam: (content: string) => {
 		return URLCheck(content);
 	},
